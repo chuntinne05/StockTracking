5bae7a3ef815c9b1a7fde260aedbf3b899aebabe
trungtin <trinhtrungtin462005@gmail.com>
2024-09-02 14:38:43 +0700

    update the bubble sort algorithm

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,12 +4,12 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split(" ");
-        int[] arr = new int[input.length];
-        sc.close();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(input[i]);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
+        sc.close();
         bubbleSort(arr);
         for (int i : arr) {
             System.out.print(i+" ");
@@ -19,12 +19,16 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
+            int swapped = 0;
             for (int j = 0; j <  array.length - i - 1; j++) {
                 if (array[j] > array[j+1]) {
                     int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
                 }
+            }
+            if (swapped == 0 ) {
+                break;
             }
         }
     }
